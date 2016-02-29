ecf9873ad32f81f49aa68981670f1efb3145f6a9
Zayan19 <finalzack19@gmail.com>
2016-02-29 14:38:26 -0500

    update

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ import os;
 def findDolphinPath():
     homePath = os.path.expanduser('~'); # this is the home path
     if sys.platform == "linux" or sys.platform == "linux2":
-        path = homePath + '/.local/share' # linux
+        path = homePath  # linux
         if (os.path.isdir(homePath + '/.local/share/dolphin-emu')): path += '/.local/share/dolphin-emu';
         else: path = homePath + '/.local/dolphin-emu';
         homePath = path;
     elif sys.platform == "darwin": homePath += "/Library/Application Support/Dolphin"
 
+    print(homePath);
     if(not (os.path.isdir(homePath))):
         print("can not find path for dolphin, are you sure it is installed??");
         print("if it is already installed then can you please store the location to the dolphin config files in the environment variable XDG_DATA_HOME");
